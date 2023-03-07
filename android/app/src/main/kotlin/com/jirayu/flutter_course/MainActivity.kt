1ac2667be5a416b1c16a1015a176d7e1e217c4bc
jirayujanlert <singto4me@gmail.com>
2023-03-07 22:02:56 +0700

    move from master to main

---
+++ android/app/src/main/kotlin/com/jirayu/flutter_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jirayu.flutter_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/CustomIcon.dart
@@ -0,0 +1,17 @@
+// ignore: duplicate_ignore
+// ignore: file_names
+// ignore_for_file: file_names
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData twitter = IconData(0xe900, fontFamily: "CustomIcons");
+  static const IconData facebook = IconData(0xe901, fontFamily: "CustomIcons");
+  static const IconData googlePlus =
+      IconData(0xe902, fontFamily: "CustomIcons");
+  static const IconData linkedin = IconData(0xe903, fontFamily: "CustomIcons");
+}
+
+class CustomColor {
+  static const kPrimaryColor = Color(0xFF17ead9);
+  static const kSecondaryColor = Color(0xFF6078ea);
+}
